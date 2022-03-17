ffa303a3bdf527b0225dc1f06f4b767e00e8f3b7
cesar curfrontend <cescursos2021@gmail.com>
2022-03-17 14:19:42 +0100

    cambios en el enviroment

--- chatMultiSala/src/environments/environment.prod.ts
+++ chatMultiSala/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAbym9-Ngel0FylLBZ9isTYDTrGeHvE8po",
+    authDomain: "chatmultisala.firebaseapp.com",
+    projectId: "chatmultisala",
+    storageBucket: "chatmultisala.appspot.com",
+    messagingSenderId: "77969872354",
+    appId: "1:77969872354:web:707bee1c97cf2a704755bb"
+  };
 };
--- chatMultiSala/src/environments/environment.ts
+++ chatMultiSala/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAbym9-Ngel0FylLBZ9isTYDTrGeHvE8po",
+    authDomain: "chatmultisala.firebaseapp.com",
+    projectId: "chatmultisala",
+    storageBucket: "chatmultisala.appspot.com",
+    messagingSenderId: "77969872354",
+    appId: "1:77969872354:web:707bee1c97cf2a704755bb"
+  };
 };
 
 /*
