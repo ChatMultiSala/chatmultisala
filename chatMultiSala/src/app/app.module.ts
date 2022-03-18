e83b2fbeb1add0067ec1514e6e2f0693ff6e9fc2
cesar curfrontend <cescursos2021@gmail.com>
2022-03-18 08:55:58 +0100

    creado componente login

--- chatMultiSala/src/app/app.module.ts
+++ chatMultiSala/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
