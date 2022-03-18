6c1822157dc9a9b8c4c07192d2c85daa272e5385
cesar curfrontend <cescursos2021@gmail.com>
2022-03-18 13:12:58 +0100

    modificacion app-routing

--- chatMultiSala/src/app/app-routing.module.ts
+++ chatMultiSala/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
 import { LoginComponent } from './login/login.component'
+import { SalaComponent } from './sala/sala.component'
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
-  //{ path: 'sala', component: SalaComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sala', component: SalaComponent }
 ]
 
 @NgModule({
